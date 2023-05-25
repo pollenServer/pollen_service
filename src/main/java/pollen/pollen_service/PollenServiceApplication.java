f587c19ebf9bb32a765824e68fa2ac7d8aad202a
Wooseobee <dntjq0807@naver.com>
2023-05-26 00:10:00 +0900

    init pollen_service

---
+++ src/main/java/pollen/pollen_service/PollenServiceApplication.java
@@ -0,0 +1,13 @@
+package pollen.pollen_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollenServiceApplication.class, args);
+	}
+
+}
