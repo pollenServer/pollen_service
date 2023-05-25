a3ae78a732b287e6f9f582e3f071b6ab20aabe79
Wooseobee <dntjq0807@naver.com>
2023-05-26 00:25:31 +0900

    refactor: 공통부분 함수로 추출

--- src/main/java/pollen/pollen_service/service/UserService.java
+++ src/main/java/pollen/pollen_service/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -20,61 +21,50 @@ public class UserService {
     @Value("${spring.service.secret_key}")
     private String SERVICEKEY;
 
+    private final String CHARSET = "UTF-8";
+
     public Object findOakPollen(String areaNo, String time) throws IOException, ParseException {
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getOakPollenRiskIdxV3"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + SERVICEKEY); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON)*/
-        urlBuilder.append("&" + URLEncoder.encode("areaNo","UTF-8") + "=" + URLEncoder.encode(areaNo, "UTF-8")); /*서울지점*/
-        urlBuilder.append("&" + URLEncoder.encode("time","UTF-8") + "=" + URLEncoder.encode(time, "UTF-8"));
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-
-        JSONParser jsonParser = new JSONParser();
-        JSONObject object = (JSONObject) jsonParser.parse(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-        conn.disconnect();
+        String builtUrl = buildUrl("getOakPollenRiskIdxV3", areaNo, time);
+        JSONObject object = getJsonObject(builtUrl);
 
         return object;
     }
+    
     public Object findPinePollen(String areaNo, String time) throws IOException, ParseException {
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getPinePollenRiskIdxV3"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + SERVICEKEY); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON)*/
-        urlBuilder.append("&" + URLEncoder.encode("areaNo","UTF-8") + "=" + URLEncoder.encode(areaNo, "UTF-8")); /*서울지점*/
-        urlBuilder.append("&" + URLEncoder.encode("time","UTF-8") + "=" + URLEncoder.encode(time, "UTF-8"));
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-
-        JSONParser jsonParser = new JSONParser();
-        JSONObject object = (JSONObject) jsonParser.parse(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-        conn.disconnect();
+        String builtUrl = buildUrl("getPinePollenRiskIdxV3", areaNo, time);
+        JSONObject object = getJsonObject(builtUrl);
 
         return object;
     }
+    
     public Object findWeedsPollen(String areaNo, String time) throws IOException, ParseException {
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getWeedsPollenRiskIdxV3"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + SERVICEKEY); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON)*/
-        urlBuilder.append("&" + URLEncoder.encode("areaNo","UTF-8") + "=" + URLEncoder.encode(areaNo, "UTF-8")); /*서울지점*/
-        urlBuilder.append("&" + URLEncoder.encode("time","UTF-8") + "=" + URLEncoder.encode(time, "UTF-8"));
-        URL url = new URL(urlBuilder.toString());
+        String builtUrl = buildUrl("getWeedsPollenRiskIdxV3", areaNo, time);
+        JSONObject object = getJsonObject(builtUrl);
+
+        return object;
+    }
+
+    private JSONObject getJsonObject(String builtUrl) throws IOException, ParseException {
+        URL url = new URL(builtUrl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
 
         JSONParser jsonParser = new JSONParser();
-        JSONObject object = (JSONObject) jsonParser.parse(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        JSONObject object = (JSONObject) jsonParser.parse(new InputStreamReader(conn.getInputStream(), CHARSET));
         conn.disconnect();
-
         return object;
+    }
+
+    public String buildUrl(String url, String areaNo, String time) throws UnsupportedEncodingException {
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/" + url); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey",CHARSET) + "=" + SERVICEKEY); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo",CHARSET) + "=" + URLEncoder.encode("1", CHARSET)); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows",CHARSET) + "=" + URLEncoder.encode("10", CHARSET)); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType",CHARSET) + "=" + URLEncoder.encode("JSON", CHARSET)); /*요청자료형식(XML/JSON)*/
+        urlBuilder.append("&" + URLEncoder.encode("areaNo",CHARSET) + "=" + URLEncoder.encode(areaNo, CHARSET)); /*서울지점*/
+        urlBuilder.append("&" + URLEncoder.encode("time",CHARSET) + "=" + URLEncoder.encode(time, CHARSET));    /*시간*/
+
+        return urlBuilder.toString();
     }
 }
