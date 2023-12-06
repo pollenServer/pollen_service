ee15e5fc4c5f05f842f925fc45fdf9c4ceef682e
Wooseobee <dntjq0807@naver.com>
2023-12-06 17:14:57 +0900

    feat: eureka 서버 등록

--- src/main/java/pollen/pollen_service/PollenServiceApplication.java
+++ src/main/java/pollen/pollen_service/PollenServiceApplication.java
@@ -2,8 +2,10 @@ package pollen.pollen_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PollenServiceApplication {
 
 	public static void main(String[] args) {
