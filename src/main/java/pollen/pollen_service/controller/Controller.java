07e84316c14dd3ecbf736866a6ad69e0ba880b23
Wooseobee <dntjq0807@naver.com>
2023-12-11 16:00:26 +0900

    fix: endpoint 변경

--- src/main/java/pollen/pollen_service/controller/Controller.java
+++ src/main/java/pollen/pollen_service/controller/Controller.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pollen.pollen_service.service.UserService;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/pollen_service")
 @RequiredArgsConstructor
 @Slf4j
 public class Controller {
