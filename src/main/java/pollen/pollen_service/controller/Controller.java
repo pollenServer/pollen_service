e32f073b6faab408b8a897f3a57a27259a67f3e5
Wooseobee <dntjq0807@naver.com>
2023-12-11 17:40:19 +0900

    fix: RequestMapping 수정

--- src/main/java/pollen/pollen_service/controller/Controller.java
+++ src/main/java/pollen/pollen_service/controller/Controller.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pollen.pollen_service.service.UserService;
@@ -13,7 +12,6 @@ import java.io.IOException;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/pollen_service")
 @RequiredArgsConstructor
 @Slf4j
 public class Controller {
